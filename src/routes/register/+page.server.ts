9064b1d41f7e5507c49deb34788c6e0d6aeab48a
Vadim Egorov <vadegor00@gmail.com>
2023-03-13 22:31:46 -0400

    register validation

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,5 +1,6 @@
 import { redirect, type ServerLoad } from "@sveltejs/kit";
 import type { Actions } from "./$types";
+import { z } from "zod";
 
 type RegisterUser = {
   username: string;
@@ -8,33 +9,91 @@ type RegisterUser = {
   passwordConfirm: string;
 };
 
-export const load: ServerLoad = (async ({ locals }) => {
+const registerSchema = z
+  .object({
+    username: z
+      .string({ required_error: "Username is required" })
+      .min(3, { message: "Username must be at least 3 characters long" })
+      .max(64, { message: "Username must be less than 64 characters" })
+      .trim(),
+    email: z
+      .string({ required_error: "Email is required" })
+      .min(3, { message: "Email is required" })
+      .max(64, { message: "Email must be less than 64 characters" })
+      .email(),
+    password: z
+      .string({ required_error: "Password is required" })
+      .min(6, { message: "Password must be at least 8 characters" })
+      .max(32, { message: "Password must be less than 32 characters" })
+      .trim(),
+    passwordConfirm: z
+      .string({ required_error: "Password is required" })
+      .min(6, { message: "Password must be at least 8 characters" })
+      .max(32, { message: "Password must be less than 32 characters" })
+      .trim(),
+    terms: z.enum(["on"], { required_error: "You must accept the terms and conditions" }),
+  })
+  .superRefine(({ passwordConfirm, password }, ctx) => {
+    if (passwordConfirm !== password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "Passwords must match",
+        path: ["password"],
+      });
+      ctx.addIssue({
+        code: "custom",
+        message: "Passwords must match",
+        path: ["passwordConfirm"],
+      });
+    }
+  });
+
+export const load: ServerLoad = async ({ locals }) => {
   if (locals.pb.authStore.isValid) {
-    throw redirect(307, '/');
+    throw redirect(307, "/");
   }
-  return {};
-});
+  return;
+};
 
 export const actions: Actions = {
   register: async ({ locals, request }) => {
     const data = Object.fromEntries(await request.formData()) as RegisterUser;
 
-    if (data.password !== data.passwordConfirm) {
-      return {
-        error: true,
-        msg: "Passwords do not match",
-      };
-    }
-
     try {
+      registerSchema.parse(data);
+      console.log(data);
+
       await locals.pb.collection("users").create(data);
       await locals.pb.collection("users").authWithPassword(data.email, data.password);
 
       locals.pb.authStore.clear();
-    } catch (err) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      const { username, email } = data;
+
+      if (err?.response?.code === 400) {
+        let errors = {};
+
+        if (err.response.data.email) {
+          errors = { ...errors, email: ["Email is already in use"] };
+        }
+
+        if (err.response.data.username) {
+          errors = { ...errors, username: ["Username is already in use"] };
+        }
+        return {
+          data: { username, email },
+          errors,
+        };
+      }
+
+      const { fieldErrors: errors } = err.flatten();
+
+      console.log({ username, email }, errors);
+
       return {
-        error: true,
-        message: err,
+        data: { username, email },
+        errors,
       };
     }
 
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+  export let form;
+</script>
+
 <div class="flex flex-col items-center h-full w-full pt-12">
   <h2 class="mt-2 text-center text-3xl font-bold text-primary">Register for an account</h2>
   <p class="text-center mt-1 text-base-100">
@@ -10,19 +14,48 @@
         <label for="username" class="label font-medium pb-1">
           <span class="label-text">Username</span>
         </label>
-        <input type="text" name="username" class="input input-bordered w-full max-w-xs" />
+        <input
+          type="text"
+          name="username"
+          class="input {form?.errors?.username ? 'input-error' : 'input-bordered'} w-full max-w-xs"
+          value={form?.data?.username ?? ""}
+        />
+        <label for="username" class="label">
+          {#if form?.errors?.username}
+            <span class="label-text-alt text-error">{form?.errors.username[0]}</span>
+          {/if}
+        </label>
       </div>
       <div class="form-control w-full max-w-xs">
         <label for="email" class="label font-medium pb-1">
           <span class="label-text">Email</span>
         </label>
-        <input type="email" name="email" class="input input-bordered w-full max-w-xs" />
+        <input
+          type="email"
+          name="email"
+          class="input {form?.errors?.email ? 'input-error' : 'input-bordered'} w-full max-w-xs"
+          value={form?.data?.email ?? ""}
+        />
+        <label for="email" class="label">
+          {#if form?.errors?.email}
+            <span class="label-text-alt text-error">{form?.errors.email[0]}</span>
+          {/if}
+        </label>
       </div>
       <div class="form-control w-full max-w-xs">
         <label for="password" class="label font-medium pb-1">
           <span class="label-text">Password</span>
         </label>
-        <input type="password" name="password" class="input input-bordered w-full max-w-xs" />
+        <input
+          type="password"
+          name="password"
+          class="input {form?.errors?.password ? 'input-error' : 'input-bordered'} w-full max-w-xs"
+        />
+        <label for="password" class="label">
+          {#if form?.errors?.password}
+            <span class="label-text-alt text-error">{form?.errors.password[0]}</span>
+          {/if}
+        </label>
       </div>
       <div class="form-control w-full max-w-xs">
         <label for="passwordConfirm" class="label font-medium pb-1">
@@ -31,10 +64,34 @@
         <input
           type="password"
           name="passwordConfirm"
-          class="input input-bordered w-full max-w-sm"
+          class="input {form?.errors?.passwordConfirm
+            ? 'input-error'
+            : 'input-bordered'} w-full max-w-sm"
         />
+        <label for="passwordConfirm" class="label">
+          {#if form?.errors?.passwordConfirm}
+            <span class="label-text-alt text-error">{form?.errors.passwordConfirm[0]}</span>
+          {/if}
+        </label>
       </div>
-      <div class="w-full max-w-xs pt-3">
+      <div class="form-control w-full max-w-xs">
+        <label class="label cursor-pointer justify-start space-x-4">
+          <input
+            type="checkbox"
+            name="terms"
+            class="checkbox checkbox-primary {form?.errors?.terms
+              ? 'border-error'
+              : 'border-primary'}"
+          />
+          <span class="label-text">I accept the terms and conditions</span>
+        </label>
+        <label for="terms" class="label">
+          {#if form?.errors?.terms}
+            <span class="label-text-alt text-error">{form?.errors.terms[0]}</span>
+          {/if}
+        </label>
+      </div>
+      <div class="w-full max-w-xs">
         <button class="btn bg-primary w-full max-w-xs text-white hover:bg-purple-700 delay-100">
           Register
         </button>
