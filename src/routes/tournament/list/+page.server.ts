6f5eb23289dbc70d0a8a180e7f8a264e276dd6f0
Vadim Egorov <vadegor00@gmail.com>
2023-03-15 19:50:23 -0400

    display list of tournaments

---
+++ src/routes/tournament/list/+page.server.ts
@@ -0,0 +1,30 @@
+import { serializeNonPOJOs } from "$lib/helpers";
+import { redirect, type ServerLoad } from "@sveltejs/kit";
+
+export const load: ServerLoad = async ({ locals }) => {
+  if (!locals.pb.authStore.isValid) {
+    throw redirect(307, "/login");
+  }
+
+  try {
+    const userId = locals.pb.authStore.model?.id;
+
+    if (!userId) {
+      throw new Error("no userId");
+    }
+
+    const user = await locals.pb
+      .collection("users")
+      .getFirstListItem(`id="${userId}"`, {
+        expand: "tournaments, tournaments.registeredUsers, tournaments.submissions",
+      });
+
+    const tournaments = serializeNonPOJOs(user.expand.tournaments);
+
+    return {
+      tournaments,
+    };
+  } catch (err) {
+    return { err };
+  }
+};
---
+++ src/routes/tournament/list/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let data;
+</script>
+
+<div>
+  {#each data.tournaments as tournament}
+    <div>
+        Title: {tournament.title}
+    </div>
+  {/each}
+</div>
