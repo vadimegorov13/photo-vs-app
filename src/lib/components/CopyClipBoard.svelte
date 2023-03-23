fec0ce69db4955e13da9904931d835641701a010
Vadim Egorov <vadegor00@gmail.com>
2023-03-23 14:51:53 -0800

    added invite link to the tournament card, and updated tournament card

---
+++ src/lib/components/CopyClipBoard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  export let inviteLink: string;
+
+  let textarea: any;
+
+  onMount(() => {
+    textarea.select();
+    document.execCommand("copy");
+  });
+</script>
+
+<textarea bind:value={inviteLink} bind:this={textarea} />
--- src/lib/components/tournament/TournamentCard.svelte
+++ src/lib/components/tournament/TournamentCard.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import { applyAction, enhance } from "$app/forms";
   import { invalidateAll } from "$app/navigation";
-  import { Icon, Trash, UserMinus } from "svelte-hero-icons";
+  import { Icon, Trash, UserMinus, Share } from "svelte-hero-icons";
   import Input from "../inputs/Input.svelte";
+  import CopyClipBoard from "../CopyClipBoard.svelte";
 
   export let tournament: any;
   export let user: any;
@@ -31,61 +32,74 @@
   const handleAlert = () => {
     tournamentAlert = !tournamentAlert;
   };
+
+  const copy = () => {
+    const app = new CopyClipBoard({
+      target: document.getElementById("clipboard") as any,
+      props: {
+        inviteLink: `http://127.0.0.1:5173/tournament/join/${tournament.expand.tournament.id}`,
+      },
+    });
+    app.$destroy();
+    handleSuccessfullyCopied();
+  };
+
+  const handleSuccessfullyCopied = () => {
+    alert(`Copide an invite link to your clipboard`);
+  };
 </script>
 
-{#if tournamentAlert && user.id === tournament.expand.tournament.host}
-  <div class="alert shadow-lg alert-error">
-    <div>
-      <Icon class="w-4 h-4" src={Trash} />
-      <span>Are you sure you want to delete this tournament?.</span>
-    </div>
-    <div class="flex-none">
-      <button on:click={handleAlert} disabled={loading} class="btn btn-sm btn-ghost">Cancel</button>
-      <form method="POST" action="?/delete" use:enhance={deleteTournament}>
-        <Input
-          id="id"
-          label="id"
-          value={tournament.expand.tournament.id}
-          disabled={loading}
-          hidden
-        />
-        <button type="submit" disabled={loading} class="btn btn-sm btn-primary">Delete</button>
-      </form>
-    </div>
-  </div>
-{:else if tournamentAlert && user.id !== tournament.expand.tournament.host}
-  <div class="alert shadow-lg alert-error">
-    <div>
-      <Icon class="w-4 h-4" src={UserMinus} />
-      <span>Are you sure you want to leave this tournament?.</span>
-    </div>
-    <div class="flex-none">
-      <button on:click={handleAlert} disabled={loading} class="btn btn-sm btn-ghost">Cancel</button>
-      <form method="POST" action="?/leave" use:enhance={deleteTournament}>
-        <Input id="id" label="id" value={tournament.id} disabled={loading} hidden />
-        <button type="submit" disabled={loading} class="btn btn-sm btn-primary">Leave</button>
-      </form>
-    </div>
-  </div>
-{/if}
-
-<div class="card md:card-side bg-base-100 shadow-lg">
+<div class="card-normal shadow-lg">
   <div class="card-body">
-    <div class="flex flex-row gap-8">
+    {#if tournamentAlert && user.id === tournament.expand.tournament.host}
+      <div class="alert shadow-lg alert-error">
+        <div>
+          <Icon class="w-16 h-16" src={Trash} />
+          <span>Are you sure you want to delete this tournament?</span>
+        </div>
+        <div class="flex flex-col">
+          <button on:click={handleAlert} disabled={loading} class="btn btn-sm btn-ghost"
+            >Cancel</button
+          >
+          <form method="POST" action="?/delete" use:enhance={deleteTournament}>
+            <Input
+              id="id"
+              label="id"
+              value={tournament.expand.tournament.id}
+              disabled={loading}
+              hidden
+            />
+            <button type="submit" disabled={loading} class="btn btn-sm btn-primary">Delete</button>
+          </form>
+        </div>
+      </div>
+    {:else if tournamentAlert && user.id !== tournament.expand.tournament.host}
+      <div class="alert shadow-lg alert-error">
+        <div>
+          <Icon class="w-4 h-4" src={UserMinus} />
+          <span>Are you sure you want to leave this tournament?</span>
+        </div>
+        <div class="flex flex-col">
+          <button on:click={handleAlert} disabled={loading} class="btn btn-sm btn-ghost"
+            >Cancel</button
+          >
+          <form method="POST" action="?/leave" use:enhance={deleteTournament}>
+            <Input id="id" label="id" value={tournament.id} disabled={loading} hidden />
+            <button type="submit" disabled={loading} class="btn btn-sm btn-primary">Leave</button>
+          </form>
+        </div>
+      </div>
+    {/if}
+    <div class="flex flex-row w-full">
       <h2 class="card-title">{tournament.expand.tournament.title}</h2>
-      {#if user.id === tournament.expand.tournament.host}
-        <div class="text-error hover:cursor-pointer">
-          <button on:click={handleAlert} disabled={loading}>
-            <Icon class="w-6 h-6" src={Trash} />
-          </button>
-        </div>
-      {:else}
-        <div class="text-error hover:cursor-pointer">
-          <button on:click={handleAlert} disabled={loading}>
-            <Icon class="w-6 h-6" src={UserMinus} />
-          </button>
-        </div>
-      {/if}
+      <div class="ml-auto text-error hover:cursor-pointer">
+        <button on:click={handleAlert} disabled={loading}>
+          <Icon
+            class="w-6 h-6"
+            src={user.id === tournament.expand.tournament.host ? Trash : UserMinus}
+          />
+        </button>
+      </div>
     </div>
 
     <p>{tournament.expand.tournament.description}</p>
@@ -101,14 +115,26 @@
       Max submissions:
       <slug>{tournament.expand.tournament.maxSubmissions}</slug>
     </p>
+    <div class="flex flex-row items-center">
+      <p>
+        Join code:
+        <slug>{tournament.expand.tournament.joinCode}</slug>
+      </p>
+      <div class="card-actions justify-end">
+        <button on:click={copy} class="btn btn-ghost btn-square"
+          ><Icon src={Share} class="w-6 h-6" /></button
+        >
+      </div>
+      <div id="clipboard" />
+    </div>
     <div class="flex flex-row">
       <p>
         You've submitted
         <slug>{tournament.submissions.length}</slug> images
       </p>
       <div class="card-actions justify-end">
-        <a sapper:prefetch rel="external" href={`/tournament/submission/${tournament.tournament}`}>
-          <button class="btn btn-primary">Submit Image</button>
+        <a rel="external" href={`/tournament/submission/${tournament.tournament}`}>
+          <button class="btn btn-accent">Submit Image</button>
         </a>
       </div>
     </div>
