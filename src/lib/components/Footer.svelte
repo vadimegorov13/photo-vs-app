eed42a68ecf50a03c3ba8a74eef5328087790df1
Vadim Egorov <vadegor00@gmail.com>
2023-04-01 23:49:09 -0800

    footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,11 +1,11 @@
 <div class="flex items-center justify-between p-4 mx-auto flex-col">
   <div class="font-semibold text-xs">
-    Built by <a href="https://github.com/vadimegorov13" class="hover:text-purple-500">
+    Built by <a href="https://github.com/vadimegorov13" class="hover:text-primary">
       Vadim Egorov
     </a>
   </div>
   <div class="font-semibold text-xs">
-    <a href="https://github.com/vadimegorov13/photo-vs-app" class="hover:text-purple-500">
+    <a href="https://github.com/vadimegorov13/photo-vs-app" class="hover:text-primary">
       GitHub repository
     </a>
   </div>
